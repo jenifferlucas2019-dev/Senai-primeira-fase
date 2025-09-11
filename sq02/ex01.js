782aea90b502ce860e3e67643e0976442202dc73
jeniffer lucas <jenifferlucas2019@gmail.com>
2025-09-10 22:23:22 -0300

    esxercio rafael

---
+++ sq02/ex01.js
@@ -0,0 +1,20 @@
+let idade
+
+// // idade = Number(prompt("digite sua idade:"))
+// if(idade >= 18) {
+//    alert("maior de idade")
+//    alert("pode sair pro intervalo")
+//    alert("pode sair pro recreio")
+// }else{
+    
+// }
+// alert("dimeno")
+// console.log(idade);
+
+// let numero
+// numero =Number(prompt("digite o numero"))
+// if(numero % 2==0){
+//       alert ("par")
+// }else{
+//     alert(" impar")
+// }
